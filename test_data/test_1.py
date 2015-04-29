110cc1d6ffda317213492ac0bfd5836ca58c1d02
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-04-29 10:40:38 +0200

    Updated test script.

--- test_data/test_1.py
+++ test_data/test_1.py
@@ -10,9 +10,9 @@ from vispy.app import run
 import phy
 from phy.cluster.manual.session import Session
 
-phy.debug()
+phy.set_level('info')
 
-path = '/home/cyrille/1_simple120sec/test_hybrid_120sec.kwik'
+# path = '/home/cyrille/1_simple120sec/test_hybrid_120sec.kwik'
 path = '/data/spikesorting/4_20141130/20141130_all.kwik'
 
 session = Session()
@@ -21,15 +21,18 @@ session.open(path)
 
 # Cluster counts.
 x = session.clustering.cluster_counts
-cl = (sorted(x.items(), key=operator.itemgetter(1))[::-1][:50])
-clu = [_[0] for _ in cl][1:4]
+cl = (sorted(x.items(), key=operator.itemgetter(1))[::-1])
+clu = [_[0] for _ in cl][1:]
 
-print(clu)
 session.select(clu)
-
-# session.show_correlograms()
-session.show_features()
 # session.show_waveforms()
+
+# session.select([9, 12, 24])
+# session.select([9, 24, 12])
+# session.show_correlograms()
+# session.show_waveforms()
+session.show_traces()
+# session.show_features()
 
 run()
 
