e4f2d51757cf24fd8a225f37eadbe2a7c880c013
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-05-11 15:55:27 +0200

    Updated script.

--- test_data/test_1.py
+++ test_data/test_1.py
@@ -8,32 +8,25 @@ import matplotlib.pyplot as plt
 from vispy.app import run
 
 import phy
-from phy.cluster.manual.session import Session
+from phy.cluster.manual import Session
 
 phy.set_level('info')
 
-# path = '/home/cyrille/1_simple120sec/test_hybrid_120sec.kwik'
-path = '/data/spikesorting/4_20141130/20141130_all.kwik'
+path = '/data/spikesorting/1_simple120sec/test_hybrid_120sec.kwik'
+# path = '/data/spikesorting/4_20141130/20141130_all.kwik'
+# path = '/data/spikesorting/for_shabnam_20150429/manual_sort_test.kwik'
 
-session = Session()
-
-session.open(path)
+session = Session(path)
+session.model.describe()
 
 # Cluster counts.
-x = session.clustering.cluster_counts
-cl = (sorted(x.items(), key=operator.itemgetter(1))[::-1])
-clu = [_[0] for _ in cl][1:]
+# x = session.clustering.cluster_counts
+# cl = (sorted(x.items(), key=operator.itemgetter(1))[::-1])
+# clu = [_[0] for _ in cl][1:5]
 
-session.select(clu)
-# session.show_waveforms()
+session.show_gui()
 
-# session.select([9, 12, 24])
-# session.select([9, 24, 12])
-# session.show_correlograms()
-# session.show_waveforms()
-session.show_traces()
-# session.show_features()
-
-run()
+# This is only necessary when using VisPy canvases exclusively, not the Qt GUI.
+# run()
 
 session.close()
