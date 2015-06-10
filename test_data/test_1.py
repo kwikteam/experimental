58437a0bb99295437fae5a032b1c0cdda6d54ccf
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-06-10 11:26:50 +0200

    WIP: read retinal data.

--- test_data/test_1.py
+++ test_data/test_1.py
@@ -10,21 +10,23 @@ from vispy.app import run
 import phy
 from phy.cluster.manual import Session
 
-phy.set_level('info')
+phy.set_level('debug')
 
 path = '/data/spikesorting/1_simple120sec/test_hybrid_120sec.kwik'
 # path = '/data/spikesorting/4_20141130/20141130_all.kwik'
-# path = '/data/spikesorting/for_shabnam_20150429/manual_sort_test.kwik'
+# path = '/data/spikesorting/nick_bug/20150428_2.kwik'
 
 session = Session(path)
 # session.model.describe()
 
 # Cluster counts.
-x = session.clustering.cluster_counts
-cl = (sorted(x.items(), key=operator.itemgetter(1))[::-1])
-clu = [_[0] for _ in cl][1:5]
+# x = session.clustering.cluster_counts
+# cl = (sorted(x.items(), key=operator.itemgetter(1))[::-1])
+# clu = [_[0] for _ in cl][1:5]
 
-gui = session.show_gui()
+with phy.qt_app():
+    gui = session.show_gui()
+    # gui.show()
 # gui.select(clu)
 
 # This is only necessary when using VisPy canvases exclusively, not the Qt GUI.
